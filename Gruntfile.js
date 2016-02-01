909035b399fc4f929e3393714dea19751e783beb
Will <will@whentheresawill.net>
2016-01-31 19:49:21 -0800

    remove cdnify

--- Gruntfile.js
+++ Gruntfile.js
@@ -329,13 +329,13 @@ module.exports = function (grunt) {
         }]
       }
     },
-
-    // Replace Google CDN references
-    cdnify: {
-      dist: {
-        html: ['<%= yeoman.dist %>/*.html']
-      }
-    },
+    //
+    // // Replace Google CDN references
+    // cdnify: {
+    //   dist: {
+    //     html: ['<%= yeoman.dist %>/*.html']
+    //   }
+    // },
 
     // Copies remaining files to places other tasks can use
     copy: {
